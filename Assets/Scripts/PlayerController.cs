8c7d3aaf51c9cb1cdd1050ccdf9e0a25f0510330
Katya Yastrebkova <katya2014ya@gmail.com>
2023-05-27 12:23:41 +0300

    start of real physics car movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // private variables
-    [SerializeField] private float speed = 10f;
+    //[SerializeField] private float speed = 10f;
+    [SerializeField] private float horsePower;
     [SerializeField] private float turnSpeed = 30f;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    public GameObject centerOfMass;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,7 +27,8 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical"+ this.name);
 
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // rotate the vehicle
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
