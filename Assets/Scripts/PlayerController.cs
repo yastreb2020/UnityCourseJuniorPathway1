e6265e50ac21a38000898e2a510cc475f1bbf146
Katya Yastrebkova <katya2014ya@gmail.com>
2023-06-12 11:54:23 +0300

    rpm and speedometers for both cars

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float turnSpeed = 30f;
     private float horizontalInput;
     private float forwardInput;
+    private float rpm;
     private Rigidbody playerRb;
     [SerializeField] private GameObject centerOfMass;
     [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    [SerializeField] List<WheelCollider> wheels;
+
+    void Awake()
+    {
+        //must give each wheel a little torque or the wheel colliders will be stuck/not work properly
+        foreach (WheelCollider w in wheels)
+            w.motorTorque = 0.000001f;
+    }
 
     void Start()
     {
@@ -28,13 +38,28 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal" + this.name);
         forwardInput = Input.GetAxis("Vertical"+ this.name);
 
-        // Move the vehicle forward
-        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
-        // rotate the vehicle
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        if (IsOnGround())
+        {
+            // Move the vehicle forward
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+            // rotate the vehicle
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
 
-        speed = playerRb.velocity.magnitude * 3.6f; //kph, for mph use 2.237f
-        speedometerText.SetText("Speed: " + Mathf.RoundToInt(speed) + " km/h");
+            speed = playerRb.velocity.magnitude * 3.6f; //kph, for mph use 2.237f
+            speedometerText.SetText("Speed: " + Mathf.RoundToInt(speed) + " km/h");
+            rpm = speed % 30 * 40;
+            rpmText.SetText($"RPM : {rpm}");
+        }
+
+    }
+
+    bool IsOnGround() {
+        int isGrounded = 0;
+        foreach (var wheel in wheels) {
+            if (wheel.isGrounded) isGrounded++;
+        }
+
+        return isGrounded > 1;
     }
 }
